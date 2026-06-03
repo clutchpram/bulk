5c82c5c3def45bff7cd8ae94e83ec758d435cd62
clutchpram <amazingbrothers2721@gmail.com>
2026-06-03 09:47:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def chat():
 
         # Generate response
         response = client.models.generate_content(
-            print("GEMINI RESPONSE:", response)
+        print("GEMINI RESPONSE:", response)
             model=MODEL_NAME,
             contents=prompt
         )
